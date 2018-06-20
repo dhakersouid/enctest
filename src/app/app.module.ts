bf4fb175c2d111a22548db437fe134cff5e099e7
Dhaker <dhaker.souid@brokins.fr>
2018-06-20 09:45:24 +0200

    Mise à jour module routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { MdpoublieComponent } from './Component/DevisComparatif/mdpoublie.compon
 import { ResiliationlaposteComponent } from './Component/DevisComparatif/Resiliationlaposte.component';
 import { EspaceClientComponent } from './Component/EspaceClient/EspaceClient.component';
 import { EcDemandesComponent } from './Component/EspaceClient/EcDemandes.component';
-import { ECpaiementprimeComponent } from './Component/EspaceClient/Ecpaiementprime.component';
+import { ECpaiementprimeComponent } from './Component/EspaceClient/ECpaiementprime.component';
 import { RenonciationComponent } from './Component/EspaceClient/Renonciation.component';
 import { UploadComponent } from './Component/EspaceClient/Upload.component';
 import { DesabonnerComponent } from './Component/EspaceClient/Desabonner.component';
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,7 +17,7 @@ import { MdpoublieComponent } from './Component/DevisComparatif/mdpoublie.compon
 import { ResiliationlaposteComponent } from './Component/DevisComparatif/Resiliationlaposte.component';
 import { EspaceClientComponent } from './Component/EspaceClient/EspaceClient.component';
 import { EcDemandesComponent } from './Component/EspaceClient/EcDemandes.component';
-import { ECpaiementprimeComponent } from './Component/EspaceClient/Ecpaiementprime.component';
+import { ECpaiementprimeComponent } from './Component/EspaceClient/ECpaiementprime.component';
 import { RenonciationComponent } from './Component/EspaceClient/Renonciation.component';
 import { UploadComponent } from './Component/EspaceClient/Upload.component';
 import { DesabonnerComponent } from './Component/EspaceClient/Desabonner.component';
@@ -99,7 +99,7 @@ const appRoutes: Routes = [
         component: EcDemandesComponent
     },
     {
-        path: 'ECpaiementprime',
+        path: 'paiementprime',
         component: ECpaiementprimeComponent
     },
     {
